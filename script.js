6b375eabed8c56f549a416381f6530fcd4487da5
biah.paixão <108343635+biahpaixao16@users.noreply.github.com>
2024-08-13 09:22:53 -0300

    bf

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAltermativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector   (".texto-resultado");
